008b7c425913493cb861354656d58ae72af67ddd
Bak Nguen <sangsangden@gmail.com>
2025-12-17 23:19:08 +0700

    fixed Android-ошибка Surface / MediaCodec / WebView

--- lib/presentation/home_page/widget/youtube_player_widget.dart
+++ lib/presentation/home_page/widget/youtube_player_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 import 'package:provider/provider.dart';
 import 'package:ya_tuber/core/app_constant.dart';
 import 'package:ya_tuber/presentation/home_page/provider/home_page_provider.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart' show YoutubePlayer;
+import 'package:youtube_player_iframe/youtube_player_iframe.dart' show YoutubePlayer, YoutubePlayerController;
 
 
 
@@ -13,13 +13,17 @@ class YoutubePlayerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final homePageProvider_listen = context.watch<HomePageProvider>();
+
+    final controller = context.select<HomePageProvider, YoutubePlayerController?>(
+  (p) => p.youtubePlayerController,
+);
     final size = MediaQuery.of(context).size;
     return Neumorphic(
       style: NeumorphicStyle(depth: AppConstant.neumoDepthNegative),
       child: SizedBox(
         width: size.width * 0.5,
         height: size.width * 0.3,
-        child: homePageProvider_listen.youtubePlayerController == null ? SizedBox() : YoutubePlayer(controller: homePageProvider_listen.youtubePlayerController!, aspectRatio: 5 / 3),
+        child: controller == null ? SizedBox() : YoutubePlayer(controller: controller!, aspectRatio: 5 / 3),
       ),
     );
   }
