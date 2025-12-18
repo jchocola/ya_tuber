45236a853f6ad26db18da47110042083afe89bad
Bak Nguen <sangsangden@gmail.com>
2025-12-18 10:39:01 +0700

    track entity

---
+++ lib/domain/entity/track_entity.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class TrackEntity extends Equatable {
+  final String videoId;
+  final String title;
+  final String subtitle;
+  TrackEntity({
+    required this.videoId,
+    required this.title,
+    required this.subtitle,
+  });
+
+  
+  @override
+  List<Object?> get props => [
+    videoId,title,subtitle
+  ];
+}
